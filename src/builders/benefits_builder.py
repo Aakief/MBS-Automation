c5aba606ff37c3acf6033eabc553ae190b0888e6
Thobile Ngwenya <Thobile.Ngwenya@oldmutual.com>
2026-08-17 09:39:12 +0200

    Changed from using pensionable salary to risk salary for benefits

--- src/builders/benefits_builder.py
+++ src/builders/benefits_builder.py
@@ -4,14 +4,14 @@ def build_benefits(investments, salary) -> BenefitOverview:
 
         accumulated_credit = investments.portfolio.accumulated_credit
         employer_benefit = 0
-        death_benefit = 4 * salary.pensionable_salary
+        death_benefit = 4 * salary.annual_risk_salary
 
         return BenefitOverview (
             accumulated_credit = accumulated_credit,
             employer_benefit = employer_benefit,
             death_benefit = death_benefit,
-            disability_benefit = 0.75 * salary.pensionable_salary,
-            spouse_assurance = 1 *  salary.pensionable_salary,
+            disability_benefit = 0.75 * salary.annual_risk_salary,
+            spouse_assurance = 1 *  salary.annual_risk_salary,
             resignation_total = accumulated_credit + employer_benefit,
             retirement_total = accumulated_credit + employer_benefit,
             death_total = accumulated_credit + death_benefit + employer_benefit
